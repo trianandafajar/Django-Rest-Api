a7affce63ac46765e747ef04aaae338beca8c9aa
trianandafajar <mail.trianandafajar@gmail.com>
2025-03-24 20:40:03 +0700

    Update URL

--- mysite/api/urls.py
+++ mysite/api/urls.py
@@ -5,7 +5,8 @@ urlpatterns = [
     path("blogposts/", views.BlogPostListCreate.as_view(), name="blogpost-view-create"),
     path(
         "blogposts/<int:pk>/",
-        views.BlogPostRetrieveUpdateDestory.as_view(),
+        views.BlogPostRetrieveUpdateDestroy.as_view(),  # Perbaikan typo
         name="update",
     ),
+    path("blogposts/delete-all/", views.BlogPostDeleteAll.as_view(), name="delete-all"),  # Tambahkan endpoint penghapusan semua post
 ]
